d9ed3669f2a719a4412e310294ed93f7838d0d9c
Chandrashekhar Korivi <14151747+sudoku007@users.noreply.github.com>
2024-05-17 15:14:15 +0100

    [MTDSA-23676] [CK] Upgrade to play 3.0 (#16)

--- test/v1/controllers/requestParsers/validators/CreateAmendUkDividendsIncomeAnnualSummaryValidatorSpec.scala
+++ test/v1/controllers/requestParsers/validators/CreateAmendUkDividendsIncomeAnnualSummaryValidatorSpec.scala
@@ -21,13 +21,14 @@ import api.mocks.MockCurrentDateTime
 import api.models.errors._
 import config.AppConfig
 import mocks.MockAppConfig
-import org.joda.time.DateTime
-import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.AnyContentAsJson
 import support.UnitSpec
 import utils.CurrentDateTime
 import v1.models.request.createAmendUkDividendsIncomeAnnualSummary.CreateAmendUkDividendsIncomeAnnualSummaryRawData
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class CreateAmendUkDividendsIncomeAnnualSummaryValidatorSpec extends UnitSpec with MockAppConfig {
 
@@ -84,7 +85,7 @@ class CreateAmendUkDividendsIncomeAnnualSummaryValidatorSpec extends UnitSpec wi
 
     implicit val appConfig: AppConfig              = mockAppConfig
     implicit val dateTimeProvider: CurrentDateTime = mockCurrentDateTime
-    val dateTimeFormatter: DateTimeFormatter       = DateTimeFormat.forPattern("yyyy-MM-dd")
+    val dateTimeFormatter: DateTimeFormatter       = DateTimeFormatter.ISO_LOCAL_DATE
 
     val validator = new CreateAmendUkDividendsIncomeAnnualSummaryValidator(appConfig: AppConfig)
 
@@ -92,8 +93,8 @@ class CreateAmendUkDividendsIncomeAnnualSummaryValidatorSpec extends UnitSpec wi
       .returns(2018)
       .anyNumberOfTimes()
 
-    MockCurrentDateTime.getDateTime
-      .returns(DateTime.parse("2021-07-29", dateTimeFormatter))
+    MockCurrentDateTime.getLocalDate
+      .returns(LocalDate.parse("2021-07-29", dateTimeFormatter))
       .anyNumberOfTimes()
 
     private val MINIMUM_YEAR = 2018
